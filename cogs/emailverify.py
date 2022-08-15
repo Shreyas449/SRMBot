bab0f1275dd0ff68671cf4dc08ffde88170bf4da
Shreyas449 <shreyasanand449@gmail.com>
2022-08-15 21:17:05 +0530

    Commands almost complete only DB checks needed.

--- cogs/emailverify.py
+++ cogs/emailverify.py
@@ -18,9 +18,9 @@ class EmailVerify(commands.Cog):
         await interaction.response.send_message(f'Hi, {interaction.user.mention}')
     
 
-    @app_commands.command(name="verify", description="Use this command to verify, this command only verifies SRM Students.")
+    @app_commands.command(name="verif", description="Use this command to verify, this command only verifies SRM Students.")
     async def verify(self ,interaction: discord.Interaction, mail:str):
-
+        await interaction.response.defer(thinking=True) 
         # if mail[-13::1] != "srmist.edu.in":
         #     await interaction.response.send_message("Sorry this command is only for verifying SRM Students. Use ____ command.")
         #     return
@@ -29,31 +29,23 @@ class EmailVerify(commands.Cog):
             ###nested if to check if already register user has verified role or now
 
         ### DB add row in verification_data
-
-        print(interaction.user.id)
-        
         uid = interaction.user.id 
         mail_to_verify = mail
         otp = random.randint(100000,1000000)
         attempts = 0
-        
-        await interaction.response.send_message("Check your email for the OTP and use /otp to complete verification")
+
+        msg = EmailMessage()
+        msg['Subject'] = "SRM'26 server" 
+        msg['From'] = self.client.BOT_EMAIL_ID                 
+        msg['To'] =  mail_to_verify
+        msg.set_content(f"Your OTP is {otp}, it will be available for another 15 minutes.")
+        with smtplib.SMTP_SSL("smtp.gmail.com",465) as smtp:
+
+            smtp.login(self.client.BOT_EMAIL_ID,self.client.BOT_EMAIL_PASSWORD)
+            smtp.send_message(msg)
 
 
-        # msg = EmailMessage()
-        # msg['Subject'] = "SRM'26 server" 
-        # msg['From'] = self.client.BOT_EMAIL_ID                 
-        # msg['To'] =  mail_to_verify
-        # msg.set_content(f"Your OTP is {otp}, it will be available for another 15 minutes.")
-        # with smtplib.SMTP_SSL("smtp.gmail.com",465) as smtp:
-
-        #     smtp.login(self.client.BOT_EMAIL_ID,self.client.BOT_EMAIL_PASSWORD)
-        #     smtp.send_message(msg)
-
-
-        await interaction.response.send_message("Check your email for the OTP and use /otp to complete verification")
-
-        # await asyncio.sleep(60*5)
+        await interaction.followup.send("Check your email for the OTP and use /otp to complete verification")
 
 
     @app_commands.command(name = "otp",description="Enter OTP here after running the /verify command.")
@@ -63,6 +55,8 @@ class EmailVerify(commands.Cog):
         # Check if user id is NOT preset in verification data
         # if interaction.user.id not in                 
             # interaction.response.send_message("Please use the /verify command to recieve OTP first.")
+
+        
         pass
 
 
